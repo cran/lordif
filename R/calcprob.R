f23ecb68c10a2101bd063d9eb56c3e73dcf8d757
Seung W. Choi <s-choi@northwestern.edu>
2009-03-08 00:00:00 +0000

    version 0.1-1

---
+++ R/calcprob.R
@@ -0,0 +1,14 @@
+`calcprob` <-
+function(ipar,theta){
+ni<-nrow(ipar) 
+maxCat<-ncol(ipar) 
+NCAT<-apply(ipar,1,function (x) sum(!is.na(x))) 
+DISC<-ipar[,"a"] 
+CB<-ipar[,paste("cb",1:(maxCat-1),sep="")] 
+pp<-array(NA,c(length(theta),ni,maxCat)) 
+for (i in 1:ni) {
+pp[,i,1:(NCAT[i])]<-probgrm(theta,DISC[i],CB[i,])
+}
+return(pp)
+}
+
---
+++ R/equate.R
@@ -0,0 +1,14 @@
+`equate` <-
+function(ipar.to,ipar.from,theta) {
+SL<-function(AK) {
+a.to<-ipar.to$a
+a.from<-ipar.from$a
+cb.to<-ipar.to[-1]
+cb.from<-ipar.from[-1]
+a.from<-a.from/AK[1]
+cb.from<-cb.from*AK[1]+AK[2]
+return(sum(tcc(a.to,cb.to,theta)-tcc(a.from,cb.from,theta))^2)
+}
+return (nlminb(c(1.0,0.0),SL,lower=c(0.5,-2.0),upper=c(2.0,2.0))$par)
+}
+
