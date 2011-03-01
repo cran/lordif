df77c360b91786f10cab3fc2d68983e1c1e492e4
Seung W. Choi <s-choi@northwestern.edu>
2011-03-01 09:53:18 +0000

    version 0.1-10

--- R/plot.lordif.R
+++ R/plot.lordif.R
@@ -7,7 +7,7 @@ return(ws)
 ndif<-sum(x$flag)
 maxcat<-ncol(x$ipar.sparse)
 if(ndif<1) stop("no DIF items present")
-dev.new(record=T)
+if (Sys.info()["sysname"]=="Windows") dev.new(record=T) else par(ask=T)
 theta<-seq(x$options$minTheta,x$options$maxTheta,x$options$inc)
 difitems<-(1:x$ni)[x$flag]
 itemnames<-row.names(x$ipar.sparse)
